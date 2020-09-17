1ec7f114ce8b21a1d69b311c091eecd2d2d2c217
YYYashvi <70328165+YYYashvi@users.noreply.github.com>
2020-09-17 08:05:14 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+    
+
+
+     function turnOn() 
+     {
+        var img = document.getElementById('myImage'); 
+        img.src ="https://www.w3schools.com/js/pic_bulbon.gif";
+    }
+
+    function turnOff() 
+    {
+        var img = document.getElementById('myImage'); 
+        img.src ="https://www.w3schools.com/js/pic_bulboff.gif";
+     }
+     
